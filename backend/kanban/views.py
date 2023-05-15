a26f61844e3e0e84e88c7042998949c263db8cfd
selimkocak <31386358+selimkocak@users.noreply.github.com>
2023-05-15 11:13:05 +0300

    frontend\src\components\kanban\KanbanBoard.js
    frontend\src\components\kanban\KanbanBoard.js

--- backend/kanban/views.py
+++ backend/kanban/views.py
@@ -1,12 +1,15 @@
 # backend\kanban\views.py
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import KanbanBoard, KanbanStage
 from .serializers import KanbanBoardSerializer, KanbanStageSerializer
 
 class KanbanBoardViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  # Require authentication for kanban board endpoints
     queryset = KanbanBoard.objects.all()
     serializer_class = KanbanBoardSerializer
 
 class KanbanStageViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  # Require authentication for kanban stage endpoints
     queryset = KanbanStage.objects.all()
     serializer_class = KanbanStageSerializer
--- frontend/src/components/Kanban/KanbanBoard.js
+++ frontend/src/components/Kanban/KanbanBoard.js
@@ -1,7 +1,7 @@
 // frontend/src/components/kanban/KanbanBoard.js
 import React, { useState, useEffect, useContext } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { getTasks } from '../../services/api';
+import { getTasks, getKanbanStages } from '../../services/api';
 import { UserContext } from '../../context/UserContext';
 import { isAuthenticated } from '../../utils/auth';
 import './KanbanBoard.css';
@@ -15,64 +15,151 @@ const KanbanBoard = () => {
       return;
     }
 
-    const adaptData = (apiData) => {
-      // Adaptation logic...
+    const fetchData = async () => {
+      try {
+        const [tasksResponse, stagesResponse] = await Promise.all([
+          getTasks(),
+          getKanbanStages(),
+        ]);
+
+        console.log('Tasks Response:', tasksResponse);
+        console.log('Stages Response:', stagesResponse);
+
+        const tasks = tasksResponse.data;
+        const stages = stagesResponse;
+
+        console.log('Tasks:', tasks);
+        console.log('Stages:', stages);
+
+        // Verilerin işlenmesi devam ediyor
+        // ...
+
+        setData({ tasks, stages });
+      } catch (error) {
+        console.error('Error fetching tasks and stages:', error);
+      }
     };
 
-    getTasks()
-      .then((taskResponse) => {
-        const adaptedData = adaptData(taskResponse.data);
-        setData(adaptedData);
-      })
-      .catch(error => {
-        console.error("Error fetching tasks:", error);
-      });
+    fetchData();
   }, [user]);
 
-  const onDragEnd = result => {
-    // Drag and drop logic...
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const sourceColumn = data.columns[source.droppableId];
+    const destinationColumn = data.columns[destination.droppableId];
+    const taskId = draggableId;
+
+    if (sourceColumn === destinationColumn) {
+      // Reorder tasks in the same column
+      const newTaskIds = Array.from(sourceColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, taskId);
+
+      const newColumn = {
+        ...sourceColumn,
+        taskIds: newTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setData(newData);
+    } else {
+      // Move task to a different column
+      const sourceTaskIds = Array.from(sourceColumn.taskIds);
+      const destinationTaskIds = Array.from(destinationColumn.taskIds);
+
+      sourceTaskIds.splice(source.index, 1);
+      destinationTaskIds.splice(destination.index, 0, taskId);
+
+      const newSourceColumn = {
+        ...sourceColumn,
+        taskIds: sourceTaskIds,
+      };
+
+      const newDestinationColumn = {
+        ...destinationColumn,
+        taskIds: destinationTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newSourceColumn.id]: newSourceColumn,
+          [newDestinationColumn.id]: newDestinationColumn,
+        },
+      };
+
+      setData(newData);
+    }
   };
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="board">
-        {data && data.columnOrder.map(columnId => {
-          const column = data.columns[columnId];
-          const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
-
-          return (
-            <div className="column" key={column.id}>
-              <h3>{column.title}</h3>
-              <Droppable droppableId={column.id}>
-                {(provided, snapshot) => (
-                  <div
-                    className="task-list"
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                  >
-                    {tasks.map((task, index) => (
-                      <Draggable key={task.id} draggableId={task.id} index={index}>
-                        {(provided, snapshot) => (
-                          <div
-                            className="task"
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >
-                            {task.content}
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
-            </div>
-          );
-        })}
+      {data.stages.map((stage) => (
+          <div className="column" key={stage.id}>
+            <h3>{stage.name}</h3>
+            <Droppable droppableId={stage.id.toString()}>
+              {(provided, snapshot) => (
+                <div
+                  className={`task-list ${snapshot.isDraggingOver ? 'dragging-over' : ''}`}
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {data.tasks.map((task, index) => {
+                    if (task.stage === stage.id) {
+                      return (
+                        <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                          {(provided, snapshot) => (
+                            <div
+                              className={`task ${snapshot.isDragging ? 'dragging' : ''}`}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <h4>{task.title}</h4>
+                              <p>{task.description}</p>
+                              <p>Assignee: {task.assignee}</p>
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        ))}
       </div>
     </DragDropContext>
   );
 };
+
 export default KanbanBoard;
