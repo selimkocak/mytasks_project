cef19538a8d7fd571696559c627f275d61bdd79f
selimkocak <31386358+selimkocak@users.noreply.github.com>
2023-05-16 18:18:36 +0300

    frontend\src\components\kanban\KanbanDragDrop.js
    frontend\src\components\kanban\KanbanDragDrop.js

--- frontend/src/components/kanban/KanbanDragDrop.js
+++ frontend/src/components/kanban/KanbanDragDrop.js
@@ -1,87 +1,74 @@
 // frontend/src/components/kanban/KanbanDragDrop.js
-import React from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
+import { useDrag } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
-const KanbanDragDrop = ({ data, setData }) => {
-  const onDragEnd = (result) => {
-    const { destination, source, draggableId } = result;
+const ItemTypes = {
+  CARD: 'card',
+};
 
-    if (!destination) {
-      return;
-    }
+const Card = ({ id, text, moveCard }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.CARD,
+    item: { id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  
+  const [, drop] = useDrop(() => ({
+    accept: ItemTypes.CARD,
+    hover(item, monitor) {
+      if (item.id !== id) {
+        moveCard(item.id, id);
+      }
+    },
+  }), [id]);
+  
+  const dragDropRef = (element) => {
+    drag(element);
+    drop(element);
+  };
 
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
+  return (
+    <div ref={dragDropRef} style={{ opacity: isDragging ? 0.5 : 1 }}>
+      {text}
+    </div>
+  );
+};
 
-    const tasks = [...data.tasks];
-    const sourceTask = tasks.find((task) => task.id === draggableId);
-    const newTasks = tasks.filter((task) => task.id !== draggableId);
 
-    if (source.droppableId === destination.droppableId) {
-      newTasks.splice(destination.index, 0, sourceTask);
-    } else {
-      const destinationTasks = [...data.tasks];
-      const destinationTask = destinationTasks.find((task) => task.id === draggableId);
-      destinationTasks.splice(destination.index, 0, { ...destinationTask });
-      newTasks.splice(source.index, 1);
-      setData({ ...data, tasks: newTasks });
-    }
+const KanbanDragDrop = () => {
+  const [cards, setCards] = useState([
+    { id: 1, text: 'Card 1' },
+    { id: 2, text: 'Card 2' },
+    { id: 3, text: 'Card 3' },
+    // Add as many cards as you want
+  ]);
 
-    setData({ ...data, tasks: newTasks });
+  const moveCard = (draggedId, hoverId) => {
+    const draggedIndex = cards.findIndex((card) => card.id === draggedId);
+    const hoverIndex = cards.findIndex((card) => card.id === hoverId);
+
+    const newCards = [...cards];
+    newCards[draggedIndex] = cards[hoverIndex];
+    newCards[hoverIndex] = cards[draggedIndex];
+
+    setCards(newCards);
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <div className="board">
-          {data.stages.map((stage) => (
-            <div className="column" key={stage.id}>
-              <h3>{stage.name}</h3>
-              <Droppable droppableId={stage.id.toString()}>
-                {(provided, snapshot) => (
-                  <div
-                    className={`task-list ${snapshot.isDraggingOver ? 'dragging-over' : ''}`}
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                  >
-                    {data.tasks.map((task, index) => {
-                      if (task.stage === stage.id) {
-                        return (
-                          <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
-                            {(provided, snapshot) => (
-                              <div
-                                className={`task ${snapshot.isDragging ? 'dragging' : ''}`}
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <h4>{task.title}</h4>
-                                <p>{task.description}</p>
-                                <p>Atanan Kişi: {task.assignee}</p>
-                              </div>
-                            )}
-                          </Draggable>
-                        );
-                      } else {
-                        return null;
-                      }
-                    })}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
-            </div>
-          ))}
-        </div>
-      </DragDropContext>
+      <div>
+        {cards.map((card) => (
+          <Card key={card.id} {...card} moveCard={moveCard} />
+        ))}
+      </div>
     </DndProvider>
   );
 };
 
 export default KanbanDragDrop;
+
